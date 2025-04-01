c92845a5b7b091a4d240aecee44f10ebe4a5166f
funnyhdss <heitor.schutz.senaisp@gmail.com>
2025-04-01 10:57:53 -0300

    feito

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import random
-
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 charadas = [
-    {'id': 1, 'charada':'O que tem boca, mas não fala?','resposta':'o fogão'},
+    {'id': 1, 'charada':'O que tem boca, mas não fala?','resposta':'O fogão'},
     {'id': 2, 'charada':'Um homem soltou dois peidos em sequência. Qual é o nome do filme?','resposta':'gás parzinho'},
-    {'id': 3, 'charada':'O que só trabalha se lhe batem na cabeça?','resposta':'o prego'},
+    {'id': 3, 'charada':'O que só trabalha se lhe batem na cabeça?','resposta':' o prego'},
     {'id': 4, 'charada':'O que é, o que é? Quando se tira o R, fica leve como uma pena.','resposta':'a perna'},
-    {'id': 5, 'charada':'Qual é a Lua que nunca esta com fome?','resposta':'lua cheia'},
+    {'id': 5, 'charada':'Qual é a Lua que nunca esta com fome?','resposta':' a lua cheia'},
     {'id': 6, 'charada':'O que o aluno fala para a professora e gosta de receber dos pais?','resposta':'presente'},
-    {'id': 7, 'charada':'Qual é o céu que não tem estrelas?','resposta':'céu da boca'},
-    {'id': 9, 'charada':'O que te pertence, mas as outras pessoas utilizam mais do que você','resposta':'nome'},
-    {'id': 10, 'charada':'O que é, o que é? Quando dizemos o seu nome, ele deixa de existir.','resposta':'o silêncio.'},
+    {'id': 7, 'charada':'Qual é o céu que não tem estrelas?','resposta':' o céu da boca'},
+    {'id': 9, 'charada':'O que te pertence, mas as outras pessoas utilizam mais do que você','resposta':' o nome'},
+    {'id': 10, 'charada':'O que é, o que é? Quando dizemos o seu nome, ele deixa de existir.','resposta':' o silêncio.'},
 ]
 
 @app.route('/')
